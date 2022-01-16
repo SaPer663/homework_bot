90113dcfdedb0463d0a27de4da9edd5245f6e8c0
SaPer663 <saper663ru@gmail.com>
2022-01-16 20:02:26 +0500

    del MissingEnvironmentVariable

--- exceptions.py
+++ exceptions.py
@@ -1,6 +1,2 @@
-class MissingEnvironmentVariable(Exception):
-    """Отсутствует переменная окружения."""
-
-
 class ResponseStatusIsNotOK(Exception):
     """Статус ответа сервера отличный от `OК`."""
